446568e475f82c3ff0d06b98aec4e6f08aa2e316
keyem4251 <kisk.emura@gmail.com>
2024-10-10 21:01:22 +0900

    経筒化

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var client *mongo.Client
+var collection *mongo.Collection
 
 type Todo struct {
 	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
@@ -23,27 +23,11 @@ type Todo struct {
 
 func main() {
 	// MongoDBに接続
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	// MongoDBの接続URIを取得
-	mongoURI := "mongodb://mongo:27017"
-
-	// MongoDB接続オプションの設定
-	clientOptions := options.Client().ApplyURI(mongoURI)
-
-	// MongoDBに接続
-	var err error
-	client, err = mongo.Connect(ctx, clientOptions)
+	client, err := connectDB()
 	if err != nil {
-		log.Fatalf("MongoDB接続エラー: %v", err)
+		log.Fatal("MongoDB接続エラー")
 	}
-
-	// MongoDBにPingして接続確認
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("MongoDB接続に失敗しました: %v", err)
-	}
+	collection = createCollection(client)
 
 	// Ginのデフォルトルーターを作成
 	r := gin.Default()
@@ -57,9 +41,41 @@ func main() {
 
 	r.POST("/todos", createTodo)
 	r.GET("/todos", getTodos)
+	r.PUT("/todos/:id", updateTodo)
+	r.DELETE("/totos/:id", deleteTodo)
 
 	// サーバーをポート8080で起動
 	r.Run(":8080")
+}
+
+func connectDB() (*mongo.Client, error) {
+	// MongoDBに接続
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// MongoDB接続オプションの設定
+	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
+
+	// MongoDBに接続
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("MongoDB接続エラー: %v", err)
+		return nil, err
+	}
+
+	// MongoDBにPingして接続確認
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("MongoDB接続に失敗しました: %v", err)
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func createCollection(c *mongo.Client) *mongo.Collection {
+	collection := c.Database("todoapp").Collection("todos")
+	return collection
 }
 
 // todoを作成
@@ -74,7 +90,6 @@ func createTodo(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	collection := client.Database("todoapp").Collection("todos")
 	result, err := collection.InsertOne(ctx, todo)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Todo作成中にエラー"})
@@ -87,7 +102,6 @@ func getTodos(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	collection := client.Database("todoapp").Collection("todos")
 	cursor, err := collection.Find(ctx, bson.M{})
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Todo取得中にエラー"})
@@ -101,4 +115,47 @@ func getTodos(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, todos)
+}
+
+func updateTodo(c *gin.Context) {
+	id := c.Param("id")
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "無効なID"})
+		return
+	}
+
+	var todo Todo
+	if err := c.BindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "無効な入力"})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := collection.UpdateOne(ctx, bson.M{"_id": objID}, bson.M{"$set": todo})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Todo更新中にエラー"})
+		return
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+func deleteTodo(c *gin.Context) {
+	id := c.Param("id")
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "無効なID"})
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := collection.DeleteOne(ctx, bson.M{"_id": objID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Todo削除中にエラー"})
+		return
+	}
+	c.JSON(http.StatusOK, result)
 }
